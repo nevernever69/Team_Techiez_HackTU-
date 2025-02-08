0503cf72e62ea9794b77c2e8e097a7516752aa9e
devashish <mshubh612@gmail.com>
2025-02-08 16:30:09 +0530

    optimisticUI

---
+++ frontend/HackTuFrontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GlowingEffectDemo from '../components/glowingEffectPage'
+import { CoverDemo } from '../components/CoverPage'
+
+
+function Home() {
+    return (
+        <>
+        <GlowingEffectDemo />
+        <CoverDemo />
+        </>
+    )
+}
+
+export default Home
